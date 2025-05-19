eef6e73bf16afb73f647e1076c907f3ad3616afc
dkvsl <dream_morfeo@icloud.com>
2025-05-19 18:59:50 +0200

    Add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'dkvsl.github.io',
+	site: 'https://dkvsl.github.io',
 	integrations: [
 		starlight({
 			title: 'Blog',
