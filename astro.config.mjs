055a75baedcf89ee1e0e91c7feb71d33abadea7d
dkvsl <dream_morfeo@icloud.com>
2025-05-19 19:27:25 +0200

    config astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dkvsl.github.io',
-	integrations: [
-		starlight({
-			title: 'Blog',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dkvsl' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://dkvsl.github.io",
+  integrations: [
+    starlight({
+      title: "Blog",
+      expressiveCode: {
+        themes: ["one-dark-pro", "one-light"],
+      },
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/dkvsl" },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
